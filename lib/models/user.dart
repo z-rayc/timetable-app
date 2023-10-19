64c2f4c351a1b323f0553b23f51590ebddfac30e
Oransj <98455426+Oransj@users.noreply.github.com>
2023-10-19 13:36:47 +0200

    Update user.dart

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,7 +1,12 @@
-import 'course.dart';
+import 'package:timetable_app/models/course.dart';
 
 class User {
-  const User(this.id, this.username, this.email, this.courses);
+  const User({
+    required this.id,
+    required this.username,
+    required this.email,
+    required this.courses,
+  });
 
   final String id;
   final String username;
