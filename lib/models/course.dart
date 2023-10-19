d38794c6f0bb0584d6bca6efd9423f9afff86b3d
Oransj <98455426+Oransj@users.noreply.github.com>
2023-10-19 13:36:44 +0200

    Update course.dart

--- lib/models/course.dart
+++ lib/models/course.dart
@@ -1,7 +1,15 @@
+import 'package:flutter/material.dart';
+
 class Course {
-  const Course(this.id, this.name, this.nameAlias);
+  const Course({
+    required this.id,
+    required this.name,
+    required this.nameAlias,
+    required this.colour,
+  });
 
   final String id;
   final String name;
   final String nameAlias;
+  final Color colour;
 }
