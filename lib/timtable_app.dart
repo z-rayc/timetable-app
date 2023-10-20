7a3a2fce849f403fe726b046cb1920b4f7ef9a7e
Oransj <98455426+Oransj@users.noreply.github.com>
2023-10-20 10:56:47 +0200

    Create timtable_app.dart

---
+++ lib/timtable_app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class TimetableApp extends ConsumerWidget {
+  const TimetableApp({super.key});
+  
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
+
+
+}
