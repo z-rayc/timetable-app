1813d00375fdd38ae26f4e2c0bd386e7cda590f9
z-rayc <zilerayser@gmail.com>
2023-10-20 12:56:27 +0200

    Update fixed typo

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,14 @@
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:timetable_app/timtable_app.dart';
+import 'package:timetable_app/timetable_app.dart';
 
 Future<void> main() async {
   await Supabase.initialize(
-      anonKey:
-          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ5d3Nsb2JlY2dkamlhY3ZwbW5iIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTc3OTE0MTksImV4cCI6MjAxMzM2NzQxOX0.rdkeFs1rpGQsZUdbGd8AHcRJnF0buxiHi9pA0nvSsOM",
-      url: "https://rywslobecgdjiacvpmnb.supabase.co");
+    anonKey:
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ5d3Nsb2JlY2dkamlhY3ZwbW5iIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTc3OTE0MTksImV4cCI6MjAxMzM2NzQxOX0.rdkeFs1rpGQsZUdbGd8AHcRJnF0buxiHi9pA0nvSsOM",
+    url: "https://rywslobecgdjiacvpmnb.supabase.co",
+  );
 
   runApp(
     const ProviderScope(
