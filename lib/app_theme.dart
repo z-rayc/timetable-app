01af56254be33525d67bd64504626df63d6efa44
Oransj <chrion@stud.ntnu.no>
2023-10-21 23:22:24 +0200

    feat: :sparkles: create AppThemes class

---
+++ lib/app_theme.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+class AppThemes {
+  static ThemeData get theme {
+    return ThemeData(
+      useMaterial3: true,
+      colorScheme: ColorScheme.fromSeed(
+        seedColor: const Color.fromRGBO(140, 20, 197, 1),
+      ).copyWith(
+        background: const Color.fromRGBO(240, 240, 240, 1),
+        secondary: const Color.fromRGBO(239, 239, 239, 1),
+      ),
+      textTheme: GoogleFonts.alataTextTheme(),
+    );
+  }
