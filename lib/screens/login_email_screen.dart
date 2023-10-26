062713e4958483c047aaae692f716dd82752680a
Kacper Lukasz Nowicki <nokacper24@gmail.com>
2023-10-26 19:08:58 +0200

    refactor: :sparkles: add form validation

--- lib/screens/login_email_screen.dart
+++ lib/screens/login_email_screen.dart
@@ -2,16 +2,36 @@ import 'package:flutter/material.dart';
 import 'package:timetable_app/app_themes.dart';
 import 'package:timetable_app/widgets/shadowed_text_form_field.dart';
 
-class LoginEmailScreen extends StatelessWidget {
-  LoginEmailScreen({super.key});
+class LoginEmailScreen extends StatefulWidget {
+  const LoginEmailScreen({super.key});
 
+  @override
+  State<LoginEmailScreen> createState() => _LoginEmailScreenState();
+}
+
+class _LoginEmailScreenState extends State<LoginEmailScreen> {
   final _formKey = GlobalKey<FormState>();
+
+  String _enteredEmail = '';
+
+  String _enteredPassword = '';
+
+  void _sublitForm() {
+    if (_formKey.currentState!.validate()) {
+      _formKey.currentState!.save();
+      // print('Email: $_enteredEmail');
+      // print('Password: $_enteredPassword');
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Processing Data')),
+      );
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Login With Email"),
+        title: const Text("Login With E-Mail"),
       ),
       body: Center(
         child: Form(
@@ -22,10 +42,20 @@ class LoginEmailScreen extends StatelessWidget {
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   const SizedBox(height: 20),
-                  const Text('Sign in'),
+                  const Text('E-mail'),
                   ShadowedTextFormField(
                     child: TextFormField(
                       decoration: AppThemes.entryFieldTheme,
+                      validator: (value) {
+                        if (value == null || value.trim().isEmpty) {
+                          return 'Please enter your e-mail';
+                        } else {
+                          return null;
+                        }
+                      },
+                      onSaved: (newValue) {
+                        _enteredEmail = newValue!;
+                      },
                     ),
                   ),
                   const SizedBox(height: 20),
@@ -34,6 +64,16 @@ class LoginEmailScreen extends StatelessWidget {
                     child: TextFormField(
                       decoration: AppThemes.entryFieldTheme,
                       obscureText: true,
+                      validator: (value) {
+                        if (value == null || value.trim().isEmpty) {
+                          return 'Please enter your password';
+                        } else {
+                          return null;
+                        }
+                      },
+                      onSaved: (newValue) {
+                        _enteredPassword = newValue!;
+                      },
                     ),
                   ),
                   const SizedBox(height: 20),
@@ -41,7 +81,7 @@ class LoginEmailScreen extends StatelessWidget {
                     children: [
                       Expanded(
                         child: ElevatedButton(
-                          onPressed: () {},
+                          onPressed: _sublitForm,
                           style: AppThemes.entryButtonTheme,
                           child: const Text('Sign in'),
                         ),
