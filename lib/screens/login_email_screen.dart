7f259e6a0516ab6c3c8d09eb89e947259e09ce0c
Kacper Lukasz Nowicki <nokacper24@gmail.com>
2023-10-26 22:13:50 +0200

    fix: :bug: optimize login with email screen for landscape

--- lib/screens/login_email_screen.dart
+++ lib/screens/login_email_screen.dart
@@ -30,82 +30,102 @@ class _LoginEmailScreenState extends State<LoginEmailScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Login With Email"),
+    final bool isWideScreen = MediaQuery.of(context).size.width > 600;
+
+    Widget signInForm = Form(
+      key: _formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const SizedBox(height: 20),
+          const Text('Email'),
+          ShadowedTextFormField(
+            child: TextFormField(
+              decoration: AppThemes.entryFieldTheme,
+              validator: (value) {
+                if (value == null || value.trim().isEmpty) {
+                  return 'Please enter your email';
+                } else {
+                  return null;
+                }
+              },
+              onSaved: (newValue) {
+                _enteredEmail = newValue!;
+              },
+            ),
+          ),
+          const SizedBox(height: 20),
+          const Text('Password'),
+          ShadowedTextFormField(
+            child: TextFormField(
+              decoration: AppThemes.entryFieldTheme,
+              obscureText: true,
+              validator: (value) {
+                if (value == null || value.trim().isEmpty) {
+                  return 'Please enter your password';
+                } else {
+                  return null;
+                }
+              },
+              onSaved: (newValue) {
+                _enteredPassword = newValue!;
+              },
+            ),
+          ),
+          const SizedBox(height: 20),
+          Row(
+            children: [
+              Expanded(
+                child: ElevatedButton(
+                  onPressed: _sublitForm,
+                  style: AppThemes.entryButtonTheme,
+                  child: const Text('Sign in'),
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
-      body: Padding(
-        padding:
-            const EdgeInsets.only(left: 70, right: 70, top: 20, bottom: 40),
-        child: Column(
-          children: [
-            Form(
-              key: _formKey,
+    );
+    var noAccountButton = Column(
+      children: [
+        const Text('No account?'),
+        ElevatedButton(
+          onPressed: () {
+            pushNewScreen(context, NavState.register);
+          },
+          style: AppThemes.entrySecondaryButtonTheme,
+          child: const Text('Register'),
+        ),
+      ],
+    );
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      appBar: AppBar(
+        title: const Text("Sign in with Email"),
+      ),
+      body: !isWideScreen
+          ? Padding(
+              padding: const EdgeInsets.only(
+                  left: 70, right: 70, top: 20, bottom: 40),
               child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const SizedBox(height: 20),
-                  const Text('Email'),
-                  ShadowedTextFormField(
-                    child: TextFormField(
-                      decoration: AppThemes.entryFieldTheme,
-                      validator: (value) {
-                        if (value == null || value.trim().isEmpty) {
-                          return 'Please enter your email';
-                        } else {
-                          return null;
-                        }
-                      },
-                      onSaved: (newValue) {
-                        _enteredEmail = newValue!;
-                      },
-                    ),
-                  ),
-                  const SizedBox(height: 20),
-                  const Text('Password'),
-                  ShadowedTextFormField(
-                    child: TextFormField(
-                      decoration: AppThemes.entryFieldTheme,
-                      obscureText: true,
-                      validator: (value) {
-                        if (value == null || value.trim().isEmpty) {
-                          return 'Please enter your password';
-                        } else {
-                          return null;
-                        }
-                      },
-                      onSaved: (newValue) {
-                        _enteredPassword = newValue!;
-                      },
-                    ),
-                  ),
-                  const SizedBox(height: 20),
-                  Row(
-                    children: [
-                      Expanded(
-                        child: ElevatedButton(
-                          onPressed: _sublitForm,
-                          style: AppThemes.entryButtonTheme,
-                          child: const Text('Sign in'),
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [signInForm, const Spacer(), noAccountButton],
+              ),
+            )
+          : SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.only(
+                    left: 150, right: 150, top: 0, bottom: 40),
+                child: Column(
+                  children: [
+                    signInForm,
+                    const SizedBox(height: 20),
+                    noAccountButton,
+                  ],
+                ),
               ),
             ),
-            const Spacer(),
-            const Text('No account?'),
-            ElevatedButton(
-              onPressed: () {
-                pushNewScreen(context, NavState.register);
-              },
-              style: AppThemes.entryButtonTheme,
-              child: const Text('Register'),
-            )
-          ],
-        ),
-      ),
     );
   }
 }
