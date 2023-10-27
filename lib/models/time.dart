fbb95254b204504c1bdc5c22fe4f8482c82e7ab1
z-rayc <zilerayser@gmail.com>
2023-10-27 18:56:26 +0200

    feat: :sparkles: create time class
    for formatting datetime

---
+++ lib/models/time.dart
@@ -0,0 +1,47 @@
+class Time {
+  const Time(this.time);
+
+  final DateTime time;
+
+  String _dateToHourMinute() {
+    return "${time.toLocal().hour.toString().padLeft(2, '0')}:${time.toLocal().minute.toString().padLeft(2, '0')}";
+  }
+
+  String _month(int month) {
+    switch (month) {
+      case 1:
+        return "January";
+      case 2:
+        return "February";
+      case 3:
+        return "March";
+      case 4:
+        return "April";
+      case 5:
+        return "May";
+      case 6:
+        return "June";
+      case 7:
+        return "July";
+      case 8:
+        return "August";
+      case 9:
+        return "September";
+      case 10:
+        return "October";
+      case 11:
+        return "November";
+      case 12:
+        return "December";
+      default:
+        throw Exception("Invalid month");
+    }
+  }
+
+  String _dateToDayMonthYear() {
+    return "${time.toLocal().day.toString().padLeft(2, '0')}. ${_month(time.toLocal().month)} ${time.toLocal().year.toString()}";
+  }
+
+  String get hourMinutes => _dateToHourMinute();
+  String get dayMonthYear => _dateToDayMonthYear();
+}
