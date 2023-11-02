664a0f6db254297989e2085d64daa17fd81d7ac0
Jakob Holkestad Molnes <Jakob.Holkestad.Molnes@gmail.com>
2023-11-02 11:33:39 +0100

    Create timetable screen for single day Fixes #7

--- lib/screens/single_day_timetable.dart
+++ lib/screens/single_day_timetable.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:timetable_app/app_themes.dart';
 import 'package:timetable_app/models/course_event.dart';
 import 'package:timetable_app/providers/timetable_provider.dart';
 
@@ -10,95 +10,53 @@ class SingleDayTimetable extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     var timetable = ref.watch(dailyTimetableProvider);
-
-    // Define the timetable start and end times
-    const startTime = 8; // 8:00 AM
-    const endTime = 18; // 6:00 PM
-
-    // Create a map to group events by course
-    final courseEvents = <String, List<CourseEvent>>{};
-    for (var event in timetable.courseEvents) {
-      if (!courseEvents.containsKey(event.course.name)) {
-        courseEvents[event.course.name] = [];
-      }
-      courseEvents[event.course.name]?.add(event);
-    }
+    // Sort events by start time
+    timetable.courseEvents.sort((a, b) => a.startTime.compareTo(b.startTime));
 
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Single day timetable'),
+        title: const Text("Event list"),
       ),
-      body: Column(
-        mainAxisSize: MainAxisSize.max,
-        children: <Widget>[
-          // Create the timetable grid
-          Expanded(
-            child: DataTable(
-              columns: const [
-                DataColumn(label: Text('Time')),
-                DataColumn(label: Text('Courses')),
-              ],
-              rows: [
-                // Generate time slots
-                for (int hour = startTime; hour <= endTime; hour++) ...[
-                  DataRow(
-                    cells: [
-                      DataCell(Text('$hour:00')),
-                      DataCell(
-                        SizedBox(
-                          height: double.infinity,
-                          child: Column(
-                            children: [
-                              // Generate course blocks for the current hour
-                              for (var courseName in courseEvents.keys)
-                                if (courseEvents[courseName]!.any((event) =>
-                                    event.startTime.hour <= hour &&
-                                    event.endTime.hour >= hour))
-                                  EventWidget(
-                                      courseName: courseName,
-                                      color: courseEvents[courseName]![0]
-                                          .course
-                                          .colour),
-                            ],
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              ],
-            ),
-          ),
-        ],
+      body: ListView.builder(
+        itemCount: timetable.courseEvents.length,
+        itemBuilder: (context, index) {
+          var courseEvent = timetable.courseEvents[index];
+          return _buildCourseEventTile(
+              courseEvent,
+              CalendarItemColour
+                  .values[index % CalendarItemColour.values.length]);
+        },
       ),
     );
   }
 }
 
-class EventWidget extends StatelessWidget {
-  const EventWidget({
-    super.key,
-    required this.courseName,
-    required this.color,
-  });
-
-  final String courseName;
-  final Color color;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      // makethe block take up the entire height of the cell
-      height: double.infinity,
-
-      decoration:
-          BoxDecoration(borderRadius: BorderRadius.circular(2.0), color: color),
-      child: Text(
-        courseName,
-        style: const TextStyle(
-          color: Colors.white,
+Widget _buildCourseEventTile(
+    CourseEvent courseEvent, CalendarItemColour colour) {
+  return Card(
+    color: courseEvent.course.colour,
+    margin: const EdgeInsets.fromLTRB(40, 8, 40, 2),
+    child: Container(
+      decoration: CalendarItemTheme.calendarDecoration(colour),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: ListTile(
+          tileColor: courseEvent.course.colour,
+          title: Text(courseEvent.course.name),
+          subtitle: Text(courseEvent.course.id),
+          trailing: hourMinute(courseEvent.startTime, courseEvent.endTime),
         ),
       ),
-    );
-  }
+    ),
+  );
+}
+
+Text hourMinute(DateTime startTime, DateTime endTime) {
+  var startHourWithPadding = startTime.hour.toString().padLeft(2, '0');
+  var startMinuteWithPadding = startTime.minute.toString().padLeft(2, '0');
+
+  var endHourWithPadding = endTime.hour.toString().padLeft(2, '0');
+  var endMinuteWithPadding = endTime.minute.toString().padLeft(2, '0');
+  return Text(
+      "$startHourWithPadding:$startMinuteWithPadding - $endHourWithPadding:$endMinuteWithPadding");
 }
