d6642b244aec5f28a9610cc3c5c917dac80e49de
z-rayc <zilerayser@gmail.com>
2023-11-06 23:44:55 +0100

    feat: :sparkles: add more form fields

--- lib/screens/create_event_screen.dart
+++ lib/screens/create_event_screen.dart
@@ -48,7 +48,7 @@ class _CreateEventScreenState extends State<CreateEventScreen> {
                       decoration: AppThemes.entryFieldTheme,
                       validator: (value) {
                         if (value == null || value.trim().isEmpty) {
-                          return 'Please enter your email';
+                          return 'Please enter a title.';
                         } else {
                           return null;
                         }
@@ -57,13 +57,80 @@ class _CreateEventScreenState extends State<CreateEventScreen> {
                     ),
                   ),
                   const SizedBox(height: 20),
+                  const Text('Description'),
+                  const SizedBox(height: 10),
+                  ShadowedTextFormField(
+                    child: TextFormField(
+                      decoration: AppThemes.entryFieldTheme,
+                      validator: (value) {
+                        if (value == null || value.trim().isEmpty) {
+                          return 'Please enter a description.';
+                        } else {
+                          return null;
+                        }
+                      },
+                      onSaved: (newValue) {},
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  const Text('Room name'),
+                  const SizedBox(height: 10),
+                  ShadowedTextFormField(
+                    child: TextFormField(
+                      decoration: AppThemes.entryFieldTheme,
+                      validator: (value) {
+                        if (value == null || value.trim().isEmpty) {
+                          return 'Please enter a room name.';
+                        } else {
+                          return null;
+                        }
+                      },
+                      onSaved: (newValue) {},
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  const Text('Building'),
+                  const SizedBox(height: 10),
+                  ShadowedTextFormField(
+                    child: TextFormField(
+                      decoration: AppThemes.entryFieldTheme,
+                      validator: (value) {
+                        if (value == null || value.trim().isEmpty) {
+                          return 'Please enter a building.';
+                        } else {
+                          return null;
+                        }
+                      },
+                      onSaved: (newValue) {},
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  const Text('Link'),
+                  const SizedBox(height: 10),
+                  ShadowedTextFormField(
+                    child: TextFormField(
+                      decoration: AppThemes.entryFieldTheme,
+                      validator: (value) {
+                        if (value == null || value.trim().isEmpty) {
+                          return 'Please enter a location link.';
+                        } else {
+                          return null;
+                        }
+                      },
+                      onSaved: (newValue) {},
+                    ),
+                  ),
+                  // TODO: Add invitees
                 ],
               ),
             ),
+            const SizedBox(height: 20),
             ElevatedButton(
-              onPressed: () {},
+              onPressed: () {
+                // TODO: Validate and submit form
+              },
               style: AppThemes.entryButtonTheme,
-              child: const Text('Register'),
+              child: const Text('Confirm'),
             ),
           ],
         ),
