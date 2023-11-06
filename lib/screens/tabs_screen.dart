13a48936bcd9cbd06e2a0b8bb85d69852e7d1367
z-rayc <zilerayser@gmail.com>
2023-11-06 23:08:30 +0100

    feat: :sparkles: update to use correct screen

--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -29,7 +29,7 @@ class _TabsScreenState extends ConsumerState<TabsScreen> {
         _selectTab(1);
         break;
       case NavDrawerChoice.myCourses:
-        pushNewScreen(context, NavState.selectCourses);
+        pushNewScreen(context, NavState.myCoursesScreen);
         break;
       case NavDrawerChoice.settings:
         pushNewScreen(context, NavState.accountSettingsScreen);
