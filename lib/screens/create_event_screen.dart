3d3ab52fd482eb1e5eb8f48aa87bb65488afd450
z-rayc <zilerayser@gmail.com>
2023-11-06 23:40:26 +0100

    feat: :sparkles: switch to Stateful and add form

--- lib/screens/create_event_screen.dart
+++ lib/screens/create_event_screen.dart
@@ -1,7 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:timetable_app/app_themes.dart';
+import 'package:timetable_app/models/user.dart';
+import 'package:timetable_app/widgets/shadowed_text_form_field.dart';
 
-class CreateEventScreen extends StatelessWidget {
+class CreateEventScreen extends StatefulWidget {
   const CreateEventScreen({super.key});
+
+  @override
+  State<CreateEventScreen> createState() {
+    return _CreateEventScreenState();
+  }
+}
+
+class _CreateEventScreenState extends State<CreateEventScreen> {
+  final _formKey = GlobalKey<FormState>();
+
+  String _enteredTitle = '';
+  String _enteredDescription = '';
+  String _enteredRoomName = '';
+  String _enteredBuildingName = '';
+  List<User> _enteredInvitees = [];
 
   @override
   Widget build(BuildContext context) {
@@ -15,6 +33,37 @@ class CreateEventScreen extends StatelessWidget {
               style: Theme.of(context).textTheme.headlineMedium!.copyWith(
                     color: Colors.black,
                   ),
+            ),
+            const SizedBox(height: 20),
+            Form(
+              key: _formKey,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const SizedBox(height: 20),
+                  const Text('Title'),
+                  const SizedBox(height: 10),
+                  ShadowedTextFormField(
+                    child: TextFormField(
+                      decoration: AppThemes.entryFieldTheme,
+                      validator: (value) {
+                        if (value == null || value.trim().isEmpty) {
+                          return 'Please enter your email';
+                        } else {
+                          return null;
+                        }
+                      },
+                      onSaved: (newValue) {},
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                ],
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              style: AppThemes.entryButtonTheme,
+              child: const Text('Register'),
             ),
           ],
         ),
