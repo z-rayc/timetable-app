d01bc782baac0cf928cf0082f24ea0402104f8c4
Oransj <chrion@stud.ntnu.no>
2023-11-07 16:51:10 +0100

    feat: :sparkles: create label

---
+++ lib/widgets/texts/label.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class CLabel extends StatelessWidget {
+  const CLabel(
+    this.text, {
+    super.key,
+    this.style = const TextStyle(),
+    this.overrideFontSize,
+  });
+
+  final String text;
+  final TextStyle style;
+  final double? overrideFontSize;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(text,
+        style: style.copyWith(
+          //if null set to 16
+          fontSize: overrideFontSize ?? 16,
+        ));
+  }
+}
