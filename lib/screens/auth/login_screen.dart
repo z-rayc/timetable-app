ce75f0b08aa245a9ba3911c918523987202a6776
Kacper Lukasz Nowicki <nokacper24@gmail.com>
2023-11-07 19:39:44 +0100

    feat: :sparkles: make google sign in button work

--- lib/screens/auth/login_screen.dart
+++ lib/screens/auth/login_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:timetable_app/main.dart';
 import 'package:timetable_app/providers/nav_provider.dart';
 import 'package:timetable_app/app_themes.dart';
 import 'package:timetable_app/widgets/login_screen/single_sign_on_button.dart';
@@ -43,12 +45,12 @@ class LoginScreen extends StatelessWidget {
                   },
                 ),
                 SingleSignOnButton(
-                  providerLogoAsset: 'assets/images/google-logo.svg',
-                  logoSemanticLabel: 'Google logo',
-                  onPressed: () {
-                    _showAlertDialog(context, 'Google sign in');
-                  },
-                ),
+                    providerLogoAsset: 'assets/images/google-logo.svg',
+                    logoSemanticLabel: 'Google logo',
+                    onPressed: () {
+                      // _showAlertDialog(context, 'Google sign in');
+                      kSupabase.auth.signInWithOAuth(Provider.google);
+                    }),
                 const Spacer(),
                 ElevatedButton(
                   style: AppThemes.entrySecondaryButtonTheme,
