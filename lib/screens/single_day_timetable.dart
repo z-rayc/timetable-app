6cd6ef2347d41c092f33f0ee293b9d78ea1717ae
Molnes <jakob@dirik.no>
2023-11-15 19:59:04 +0100

    Refactor course event tile into its own widget solves #41

--- lib/screens/single_day_timetable.dart
+++ lib/screens/single_day_timetable.dart
@@ -2,8 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:timetable_app/app_themes.dart';
 import 'package:timetable_app/models/course_event.dart';
+import 'package:timetable_app/models/event.dart';
 import 'package:timetable_app/providers/timetable_provider.dart';
 import 'package:timetable_app/screens/event_details_screen.dart';
+import 'package:timetable_app/widgets/course_event_card.dart';
 
 class SingleDayTimetable extends ConsumerWidget {
   const SingleDayTimetable({super.key});
@@ -33,11 +35,7 @@ class SingleDayTimetable extends ConsumerWidget {
             itemCount: timetable.asData!.value.courseEvents.length,
             itemBuilder: (context, index) {
               var courseEvent = timetable.asData!.value.courseEvents[index];
-              return _buildCourseEventTile(
-                  context,
-                  courseEvent,
-                  CalendarItemColour
-                      .values[index % CalendarItemColour.values.length]);
+              return CourseEventClass(event: courseEvent);
             },
           ),
         );
@@ -48,44 +46,4 @@ class SingleDayTimetable extends ConsumerWidget {
       return const CircularProgressIndicator();
     });
   }
-}
-
-// TODO extract into its own widget...
-Widget _buildCourseEventTile(
-    BuildContext context, CourseEvent courseEvent, CalendarItemColour colour) {
-  return Card(
-    color: courseEvent.course.colour,
-    margin: const EdgeInsets.fromLTRB(40, 8, 40, 2),
-    child: InkWell(
-      onTap: () {
-        Navigator.of(context).push(MaterialPageRoute(
-          builder: (context) => EventDetailsScreen(
-            event: courseEvent,
-          ),
-        ));
-      },
-      child: Container(
-        decoration: CalendarItemTheme.calendarDecoration(colour),
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: ListTile(
-            tileColor: courseEvent.course.colour,
-            title: Text(courseEvent.course.name),
-            subtitle: Text(courseEvent.course.id),
-            trailing: hourMinute(courseEvent.startTime, courseEvent.endTime),
-          ),
-        ),
-      ),
-    ),
-  );
-}
-
-Text hourMinute(DateTime startTime, DateTime endTime) {
-  var startHourWithPadding = startTime.hour.toString().padLeft(2, '0');
-  var startMinuteWithPadding = startTime.minute.toString().padLeft(2, '0');
-
-  var endHourWithPadding = endTime.hour.toString().padLeft(2, '0');
-  var endMinuteWithPadding = endTime.minute.toString().padLeft(2, '0');
-  return Text(
-      "$startHourWithPadding:$startMinuteWithPadding - $endHourWithPadding:$endMinuteWithPadding");
 }
