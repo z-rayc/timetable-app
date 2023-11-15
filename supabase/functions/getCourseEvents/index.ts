56a4dd16bc3e44c5dd2445cc5e728110c5c1afa5
Jakob Holkestad Molnes <Jakob.Holkestad.Molnes@gmail.com>
2023-11-15 10:39:19 +0100

    Fix teaching-summary key to teaching_summary in getCourseEvents function

--- supabase/functions/getCourseEvents/index.ts
+++ supabase/functions/getCourseEvents/index.ts
@@ -147,7 +147,7 @@ Deno.serve(async (req: Request) => {
           id: event.eventid,
           roomid: event.room[0].id,
           staffid: event.staffs[0].id,
-          "teaching-summary": event.summary,
+          "teaching_summary": event.summary,
         }
       })
 
