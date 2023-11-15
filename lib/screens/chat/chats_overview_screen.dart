cafd336c17a8441daff24cffb3c892a99bf02284
Kacper Lukasz Nowicki <nokacper24@gmail.com>
2023-11-15 16:40:19 +0100

    feat: add chatroom tile and add chat screen

--- lib/screens/chat/chats_overview_screen.dart
+++ lib/screens/chat/chats_overview_screen.dart
@@ -1,14 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:timetable_app/models/chat_room.dart';
+import 'package:timetable_app/screens/chat/chat_screen.dart';
+import 'package:timetable_app/widgets/chat/chat_room_tile.dart';
 
 class ChatsOverviewScreen extends StatelessWidget {
   const ChatsOverviewScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Chats'),
-      ),
-    );
+    const chatroom = sampleChatRoom;
+    return Scaffold(
+        body: ListView.builder(
+      itemCount: 1,
+      itemBuilder: (context, index) {
+        return ChatRoomTile(
+            chatRoom: chatroom,
+            onTap: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (context) {
+                    return const ChatScreen(chatRoom: chatroom);
+                  },
+                ),
+              );
+            });
+      },
+    ));
   }
 }
