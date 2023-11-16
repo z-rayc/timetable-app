7d5217d555c9783e6c114db5147e7f170e55de1f
Kacper Lukasz Nowicki <nokacper24@gmail.com>
2023-11-16 11:18:37 +0100

    feat: :sparkles: load chatrooms from database

--- lib/models/chat_room.dart
+++ lib/models/chat_room.dart
@@ -1,17 +1,14 @@
 class ChatRoom {
   final String id;
   final String name;
-  final List<String> memberIds;
 
   const ChatRoom({
     required this.id,
     required this.name,
-    required this.memberIds,
   });
 }
 
 const sampleChatRoom = ChatRoom(
   id: '1',
   name: 'Sample Chat Room',
-  memberIds: ['1', '2'],
 );
--- lib/screens/chat/chats_overview_screen.dart
+++ lib/screens/chat/chats_overview_screen.dart
@@ -1,30 +1,82 @@
 import 'package:flutter/material.dart';
+import 'package:timetable_app/main.dart';
 import 'package:timetable_app/models/chat_room.dart';
 import 'package:timetable_app/screens/chat/chat_screen.dart';
 import 'package:timetable_app/widgets/chat/chat_room_tile.dart';
 
-class ChatsOverviewScreen extends StatelessWidget {
+class ChatsOverviewScreen extends StatefulWidget {
   const ChatsOverviewScreen({super.key});
+
+  @override
+  State<ChatsOverviewScreen> createState() => _ChatsOverviewScreenState();
+}
+
+class _ChatsOverviewScreenState extends State<ChatsOverviewScreen> {
+  List<ChatRoom> _chatRooms = [];
+  bool _isLoading = true;
+
+  void _setLoading(bool loading) {
+    setState(() {
+      _isLoading = loading;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchChatRooms();
+  }
+
+  void _fetchChatRooms() async {
+    List<dynamic> chatRoomsDynamic = await kSupabase
+        .from('ChatRoomMember')
+        .select('userid, chatroomid, ChatRoom(id, name)')
+        .filter('userid', 'eq', kSupabase.auth.currentUser!.id);
+
+    List<ChatRoom> tempChatRooms = [];
+
+    for (var row in chatRoomsDynamic) {
+      tempChatRooms.add(
+        ChatRoom(
+          id: row['ChatRoom']['id'].toString(),
+          name: row['ChatRoom']['name'],
+        ),
+      );
+    }
+
+    setState(() {
+      _chatRooms = tempChatRooms;
+    });
+    _setLoading(false);
+  }
 
   @override
   Widget build(BuildContext context) {
     const chatroom = sampleChatRoom;
     return Scaffold(
-        body: ListView.builder(
-      itemCount: 1,
-      itemBuilder: (context, index) {
-        return ChatRoomTile(
-            chatRoom: chatroom,
-            onTap: () {
-              Navigator.of(context).push(
-                MaterialPageRoute(
-                  builder: (context) {
-                    return const ChatScreen(chatRoom: chatroom);
-                  },
-                ),
-              );
-            });
-      },
-    ));
+        body: _isLoading
+            ? const Center(
+                child: CircularProgressIndicator(),
+              )
+            : _chatRooms.isEmpty
+                ? const Center(
+                    child: Text('No chats yet! Create one!'),
+                  )
+                : ListView.builder(
+                    itemCount: _chatRooms.length,
+                    itemBuilder: (context, index) {
+                      return ChatRoomTile(
+                          chatRoom: _chatRooms[index],
+                          onTap: () {
+                            Navigator.of(context).push(
+                              MaterialPageRoute(
+                                builder: (context) {
+                                  return const ChatScreen(chatRoom: chatroom);
+                                },
+                              ),
+                            );
+                          });
+                    },
+                  ));
   }
 }
