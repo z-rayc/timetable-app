305df4eedd7bd5f5a78b8b80599a169858d002e8
Molnes <jakob@dirik.no>
2023-11-20 20:58:17 +0100

    move timetables into timetables folder

--- lib/screens/timetables/timetable_screen.dart
+++ lib/screens/timetables/timetable_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:timetable_app/screens/single_day_timetable.dart';
-import 'package:timetable_app/screens/week_timetable.dart';
+import 'package:timetable_app/screens/timetables/single_day_timetable.dart';
+import 'package:timetable_app/screens/timetables/week_timetable.dart';
 
 class TimetableScreen extends StatelessWidget {
   const TimetableScreen({super.key});
@@ -8,8 +8,6 @@ class TimetableScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     bool isWide = MediaQuery.of(context).size.width > 600;
-    return !isWide
-        ? const SingleDayTimetable()
-        : const WeekTimeTable();
+    return !isWide ? const SingleDayTimetable() : const WeekTimeTable();
   }
 }
