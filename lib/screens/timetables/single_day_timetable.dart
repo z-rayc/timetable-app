e5de9ee9c077bb79d73146b8284caff95e12f4e4
Jakob Holkestad Molnes <Jakob.Holkestad.Molnes@gmail.com>
2023-11-22 10:13:40 +0100

    feat: :sparkles: Add datepicker

--- lib/screens/timetables/single_day_timetable.dart
+++ lib/screens/timetables/single_day_timetable.dart
@@ -11,53 +11,53 @@ class SingleDayTimetable extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     var timetable = ref.watch(dailyTimetableProvider);
 
-    return timetable.when(data: (DailyTimetable data) {
-      if (data.courseEvents.isEmpty) {
-        return Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Text("No events today or no courses added"),
-              const SizedBox(height: 20),
-              ElevatedButton.icon(
-                  onPressed: () {
-                    ref.invalidate(myCoursesProvider);
-                    ref.invalidate(dailyTimetableProvider);
+    return Expanded(
+      child: timetable.when(data: (DailyTimetable data) {
+        if (data.courseEvents.isEmpty) {
+          return Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text("No events today or no courses added"),
+                const SizedBox(height: 20),
+                ElevatedButton.icon(
+                    onPressed: () {
+                      ref.invalidate(myCoursesProvider);
+                      ref.invalidate(dailyTimetableProvider);
 
-                    ScaffoldMessenger.of(context).showSnackBar(
-                        const SnackBar(content: Text("Refreshed")));
-                  },
-                  icon: const Icon(Icons.refresh),
-                  label: const Text("Refresh"))
-            ],
-          ),
-        );
-      } else {
-        // sort events by start time and date (earliest first)
-        data.courseEvents.sort((a, b) {
-          if (a.startTime.isBefore(b.startTime)) {
-            return -1;
-          } else if (a.startTime.isAfter(b.startTime)) {
-            return 1;
-          } else {
-            return 0;
-          }
-        });
-        return Scaffold(
-          body: ListView.builder(
+                      ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(content: Text("Refreshed")));
+                    },
+                    icon: const Icon(Icons.refresh),
+                    label: const Text("Refresh"))
+              ],
+            ),
+          );
+        } else {
+          // sort events by start time and date (earliest first)
+          data.courseEvents.sort((a, b) {
+            if (a.startTime.isBefore(b.startTime)) {
+              return -1;
+            } else if (a.startTime.isAfter(b.startTime)) {
+              return 1;
+            } else {
+              return 0;
+            }
+          });
+          return ListView.builder(
             padding: const EdgeInsets.symmetric(vertical: 10),
             itemCount: timetable.asData!.value.courseEvents.length,
             itemBuilder: (context, index) {
               var courseEvent = timetable.asData!.value.courseEvents[index];
               return CourseEventClass(event: courseEvent);
             },
-          ),
-        );
-      }
-    }, error: (Object error, StackTrace stackTrace) {
-      return SingleChildScrollView(child: Text("Error: $error, $stackTrace"));
-    }, loading: () {
-      return const Center(child: CircularProgressIndicator());
-    });
+          );
+        }
+      }, error: (Object error, StackTrace stackTrace) {
+        return SingleChildScrollView(child: Text("Error: $error, $stackTrace"));
+      }, loading: () {
+        return const Center(child: CircularProgressIndicator());
+      }),
+    );
   }
 }
--- lib/screens/timetables/timetable_screen.dart
+++ lib/screens/timetables/timetable_screen.dart
@@ -2,12 +2,76 @@ import 'package:flutter/material.dart';
 import 'package:timetable_app/screens/timetables/single_day_timetable.dart';
 import 'package:timetable_app/screens/timetables/week_timetable.dart';
 
-class TimetableScreen extends StatelessWidget {
+class TimetableScreen extends StatefulWidget {
   const TimetableScreen({super.key});
+
+  @override
+  State<TimetableScreen> createState() => _TimetableScreenState();
+}
+
+class _TimetableScreenState extends State<TimetableScreen> {
+  DateTime selectedDate = DateTime.now();
+
+  Future<void> _selectDate(BuildContext context) async {
+    final DateTime? picked = await showDatePicker(
+        context: context,
+        initialDate: selectedDate,
+        firstDate: DateTime.now().add(const Duration(days: -365)),
+        lastDate: DateTime.now().add(const Duration(days: 365)));
+    if (picked != null && picked != selectedDate) {
+      setState(() {
+        selectedDate = picked;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     bool isWide = MediaQuery.of(context).size.width > 600;
-    return !isWide ? const SingleDayTimetable() : const WeekTimeTable();
+    return Column(
+      children: [
+        _datepicker(context),
+        !isWide ? const SingleDayTimetable() : const WeekTimeTable(),
+      ],
+    );
+  }
+
+  Widget _datepicker(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "${selectedDate.day}/${selectedDate.month}/${selectedDate.year}",
+              style: Theme.of(context).textTheme.titleLarge,
+            ),
+            TextButton(
+              onPressed: () => _selectDate(context),
+              child: const Text('Select date'),
+            ),
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextButton(
+              onPressed: () => setState(() =>
+                  selectedDate = selectedDate.add(const Duration(days: -1))),
+              child: const Text('Yesterday'),
+            ),
+            TextButton(
+              onPressed: () => setState(() => selectedDate = DateTime.now()),
+              child: const Text('Today'),
+            ),
+            TextButton(
+              onPressed: () => setState(() =>
+                  selectedDate = selectedDate.add(const Duration(days: 1))),
+              child: const Text('Tomorrow'),
+            ),
+          ],
+        ),
+      ],
+    );
   }
 }
