2f3c7b9aead85aa189fc4fc74d72a757b01c0e72
z-rayc <zilerayser@gmail.com>
2023-11-24 19:26:03 +0100

    feat: :sparkles: make location optional and rename title to name

--- lib/models/custom_event.dart
+++ lib/models/custom_event.dart
@@ -3,29 +3,29 @@ import 'package:timetable_app/models/location.dart';
 
 class CustomEvent extends Event {
   const CustomEvent({
-    required super.id,
+    required super.id, // Is actually a number in the database
     required super.startTime,
     required super.endTime,
     required this.name,
     required this.description,
-    required this.location,
     required this.creatorId,
+    this.location,
   });
 
   final String name;
   final String description;
-  final Location? location;
   final String creatorId;
+  final Location? location;
 
   static CustomEvent fromJson(dynamic json) {
     return CustomEvent(
       id: json['id'],
-      name: json['title'],
+      name: json['name'],
       description: json['description'],
       startTime: DateTime.parse(json['start_time']),
       endTime: DateTime.parse(json['end_time']),
       creatorId: (json['creator']),
-      location: Location.fromJson(json['location']),
+      // location: Location.fromJson(json['location']),
     );
   }
 }
