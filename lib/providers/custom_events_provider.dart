7c80c10ed6331bc9b3dba2f3fb10bed697d049df
z-rayc <zilerayser@gmail.com>
2023-11-24 23:06:52 +0100

    fix: :bug: set up provider to get correct data

--- lib/providers/custom_events_provider.dart
+++ lib/providers/custom_events_provider.dart
@@ -1,6 +1,4 @@
 import 'dart:async';
-import 'dart:developer';
-
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:timetable_app/main.dart';
 import 'package:timetable_app/models/custom_event.dart';
@@ -16,13 +14,17 @@ class CustomEventsNotifier extends AsyncNotifier<CustomEvents> {
   FutureOr<CustomEvents> build() async {
     final db = kSupabase.rest;
 
-    // Get all the events that the user has access to
-    /* List<Map<String, dynamic>> events =
-        await db.from('CustomEvents').select('*'); */
-    List<Map<String, dynamic>> events = await db
-        .from('CustomEvents')
-        .select('*, CustomEventsMember!event_id(*)');
-    // .eq('creator', kSupabase.auth.currentUser!.id);
+    // Get all the IDs of events that the user has access to.
+    List<Map<String, dynamic>> eventsWithAccess =
+        await db.from('CustomEventsMember').select('*');
+    List<int> eventIdsList = [];
+    for (var event in eventsWithAccess) {
+      eventIdsList.add(event['event_id']);
+    }
+
+    // Get all the events that the user has access to.
+    List<dynamic> events =
+        await db.from('CustomEvents').select('*').in_('id', eventIdsList);
 
     return CustomEvents(customEvents: await convertToCustomEvents(events));
   }
@@ -33,12 +35,10 @@ final customEventsProvider =
   return CustomEventsNotifier();
 });
 
-Future<List<CustomEvent>> convertToCustomEvents(
-    List<Map<String, dynamic>> events) async {
+Future<List<CustomEvent>> convertToCustomEvents(List<dynamic> events) async {
   List<CustomEvent> customEvents = [];
 
   for (var event in events) {
-    log("Event from DB: $event");
     customEvents.add(
       CustomEvent.fromJson(event),
     );
