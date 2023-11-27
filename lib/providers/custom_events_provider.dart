4c18c9f6a66215cfcb9d75caedd783ecef948159
z-rayc <zilerayser@gmail.com>
2023-11-27 22:19:11 +0100

    feat: :sparkles: add "add custom event" function with Supabase
    uses Supabase edge function

--- lib/providers/custom_events_provider.dart
+++ lib/providers/custom_events_provider.dart
@@ -3,6 +3,15 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:timetable_app/main.dart';
 import 'package:timetable_app/models/custom_event.dart';
 
+class CustomEventsFetchError {
+  final String message;
+  CustomEventsFetchError(this.message);
+  @override
+  String toString() {
+    return message;
+  }
+}
+
 class CustomEvents {
   CustomEvents({required this.customEvents});
 
@@ -10,6 +19,41 @@ class CustomEvents {
 }
 
 class CustomEventsNotifier extends AsyncNotifier<CustomEvents> {
+  Future<CustomEventsFetchError?> addCustomEvent(
+      String name,
+      String description,
+      DateTime startTime,
+      DateTime endTime,
+      String creatorId,
+      String room,
+      String building,
+      String link,
+      List<String> memberEmails) async {
+    CustomEventsFetchError? maybeError;
+    state = const AsyncValue.loading();
+    try {
+      var response =
+          await kSupabase.functions.invoke('createCustomEvent', body: {
+        'name': name,
+        'description': description,
+        'start_time': startTime,
+        'end_time': endTime,
+        'creatorId': creatorId,
+        'room': room,
+        'building': building,
+        'link': link,
+        'memberEmails': memberEmails,
+      }).timeout(kDefaultTimeout);
+
+      if (response.status != 201) {
+        maybeError = CustomEventsFetchError(response.data['error']);
+      }
+    } catch (e, stack) {
+      state = AsyncValue.error(e, stack);
+    }
+    return maybeError;
+  }
+
   @override
   FutureOr<CustomEvents> build() async {
     final db = kSupabase.rest;
