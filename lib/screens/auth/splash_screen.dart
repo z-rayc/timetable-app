c683485d0953fde0267b960eba12b55cd8cca274
z-rayc <zilerayser@gmail.com>
2023-11-28 16:02:16 +0100

    chore: :fire: clean code

--- lib/screens/auth/splash_screen.dart
+++ lib/screens/auth/splash_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flutter_svg/svg.dart';
 import 'package:timetable_app/app_themes.dart';
 
 class SplashScreen extends ConsumerWidget {
