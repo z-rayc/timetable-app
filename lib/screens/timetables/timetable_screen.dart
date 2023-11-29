1ded3c3499ec286ee9338d8f0639285d4312f6ba
z-rayc <zilerayser@gmail.com>
2023-11-29 12:10:14 +0100

    fix: :bug: update import

--- lib/screens/timetables/timetable_screen.dart
+++ lib/screens/timetables/timetable_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:timetable_app/providers/selected_day_provider.dart';
-import 'package:timetable_app/screens/weekTimetableManager.dart';
+import 'package:timetable_app/screens/timetables/week_timetable.dart';
 import 'package:timetable_app/screens/timetables/single_day_timetable.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
